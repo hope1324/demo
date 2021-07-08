e8641ffaf0cafd14db1d0ed83cb9681636fd6009
hope1324 <supernan502@sohu.com>
2021-07-08 21:04:48 +0800

    Add WSGI support

--- server.py
+++ server.py
@@ -11,6 +11,9 @@ class index:
         data = web.input(name=None)
         return render.index(data.name)
 
+app = web.application(urls, globals())
+wsgiapp = app.wsgifunc()
+
 if __name__ == '__main__':
-    app = web.application(urls, globals())
+    
     app.run()
